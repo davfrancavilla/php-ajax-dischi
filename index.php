b50a30540f1dd7278fe8cef0e1a6838668b6b788
Davide Francavilla <davfrancavilla@gmail.com>
2020-09-18 16:23:06 +0200

    Prima milestone completata (senza stile)

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+require "db.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dischi</title>
+</head>
+<body>
+    <nav></nav>
+    <main>
+        <div class="container">
+            <?php foreach ($database as $disc){ ?>
+                <div class="disc">
+                    <img src="<?php echo $disc["poster"] ?>" alt="<?php echo $disc["title"] ?>-poster">
+                    <div class="title"><?php echo $disc["title"] ?></div>
+                    <div class="artist"><?php echo $disc["author"] ?></div>
+                    <div class="year"><?php echo $disc["year"] ?></div>
+                </div>
+            <?php }?>
+        </div>
+    </main>
+</body>
+</html>
