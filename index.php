92acf174323a8357da6358a4157457b861814997
Davide Francavilla <davfrancavilla@gmail.com>
2020-09-18 17:04:59 +0200

    Aggiunto stile

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ require "db.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dischi</title>
+    <link rel="stylesheet" href="dist/app.css">
 </head>
 <body>
     <nav></nav>
